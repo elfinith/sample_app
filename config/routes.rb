2ed8fe464b3c1817e848c54ef391ddd451d4538a
elfinith <elfinith@mail.ru>
2015-01-27 12:33:46 +0000

    Chapter 8.1.2 (работает)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   match '/signup',	to: 'users#new',		via: 'get'
-  match '/signin',	to: 'sesions#new',		via: 'get'
+  match '/signin',	to: 'sessions#new',		via: 'get'
   match '/signout',	to: 'sessions#destroy',		via: 'delete'
   match '/help',	to: 'static_pages#help',	via: 'get'
   match '/about',	to: 'static_pages#about',	via: 'get'
